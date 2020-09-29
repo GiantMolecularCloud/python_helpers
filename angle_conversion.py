3e66d5e3c2fe574b9f291613c1eada6bbfb114da
GiantMolecularCloud <26749617+GiantMolecularCloud@users.noreply.github.com>
2020-09-29 16:46:48 +0200

    add M82

--- angle_conversion.py
+++ angle_conversion.py
@@ -28,6 +28,8 @@ def angle_to_parsec(angle, source=None, distance=None):
         distance = 3.5*u.Mpc
     if ( source == 'GC' ):
         distance = 8178*u.pc
+    if ( source == 'M82' ):
+        distance = 3.6*u.Mpc
 
     return (distance*np.sin(angle)).to(u.pc)
 
@@ -58,6 +60,8 @@ def parsec_to_angle(parsec, source=None, distance=None):
         distance = 3.5*u.Mpc
     if ( source == 'GC' ):
         distance = 8178*u.pc
+    if ( source == 'M82' ):
+        distance = 3.6*u.Mpc
 
     return (np.sin((parsec/distance).to(u.dimensionless_unscaled).value)*u.radian).to(u.degree)
 
